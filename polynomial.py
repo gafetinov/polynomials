768996fd99bc1ef4c8712c256ccef07cfcf1c6d6
Artem Gafetinov <artem.gafetinov@gmail.com>
2018-05-11 19:03:23 +0500

    some changing

--- polynomial.py
+++ polynomial.py
@@ -8,6 +8,7 @@ class Polynomial():
         i = 0
         if input.isdigit():
             exponents.append(0)
+        input = input.lstrip('123456789+-')
         while i < len(input):
             if input[i].isdigit():
                 exponent = ''
@@ -17,9 +18,9 @@ class Polynomial():
                 exponents.append(int(exponent))
             elif input[i].isalpha():
                 variables.append(str(input[i]))
-                i += 1
-                if i+1 < len(input) and input[i+1] != '^':
+                if i == len(input)-1 or input[i+1] != '^':
                     exponents.append(1)
+                i += 1
             else:
                 i += 1
         return exponents, variables
@@ -30,25 +31,24 @@ class Polynomial():
         else:
             return string[2:] and string[0]
 
-    def simplify(self):
-
-        def multiply(numbers):
-            multiple = 1
-            for number in numbers:
-                multiple *= multiple
-            return multiple
-
+    def get_monomials(self, polinom):
         monomials = []
-        operators = []
         monomial_begin = 0
-        for i in range(len(self.string)):
-            if self.string[i] == '+' or '-':
-                monomials.append(self.string[monomial_begin:i])
-                operators.append(self.string[i])
-                monomial_begin = i+1
-        operators.append('')  # Для того, чтобы добавить к последнему
-        # одночлену пустую строку
-        monomials.append(self.string[monomial_begin:])
+        for i in range(len(polinom)):
+            if polinom[i] == '+' or polinom[i] == '-':
+                monomials.append(polinom[monomial_begin:i])
+                monomial_begin = i
+        monomials.append(polinom[monomial_begin:])
+        return monomials
+
+    def multiply(self, numbers):
+        multiple = 1
+        for number in numbers:
+            multiple *= number
+        return multiple
+
+    def simplify(self):
+        monomials = self.get_monomials(self.string)
         simple_monomials = []
         monomial_index = 0
         for monomial in monomials:
@@ -57,34 +57,54 @@ class Polynomial():
             i = 0
             while i < len(monomial):
                 if monomial[i].isdigit():
-                    multiplier = monomial[i]
-                    j = i+1
-                    while monomial[j].isdigit():
+                    multiplier = ''
+                    j = i
+                    while j < len(monomial) and monomial[j].isdigit():
                         multiplier += monomial[j]
                         j += 1
                     multipliers.append(multiplier)
-                    i = j
-                if monomial[i].isalpha():
-                    if monomial[i+1] != '^':
-                        variables.append(monomial[i])
-                    else:
-                        variable = monomial[i:i+2]
+                    i = j - 1
+                elif monomial[i] == '-':
+                    multipliers.append(-1)
+                elif monomial[i].isalpha():
+                    variable = monomial[i]
+                    if i < len(monomial)-1 and monomial[i+1] == '^':
+                        variable += monomial[i:i+2]
                         j = i + 2
-                        exponent = ''
                         while monomial[j].isdigit():
                             variable += monomial[j]
                             j += 1
-                        variables.append(variable)
                         i = j
+                    variables.append(variable)
                 i += 1
             variables.sort(key=self.sort_by_variables)
-            common_multiplier = multiply(multipliers)
+            variables = self.simplify_monomial(variables)
+            variables.sort(key=self.sort_by_variables)
+            common_multiplier = self.multiply(multipliers)
             simple_monomials.append(str(common_multiplier)+''.join(variables))
             monomial_index += 1
         simple_monomials.sort(key=self.sort_by_monomial, reverse=True)
+        self.string = '+'.join(simple_monomials)
 
-    def simplify_monomial(self, monomial):
-        pass
+    def simplify_monomial(self, variables):
+        simple_variables = []
+        i = 0
+        while i < len(variables):
+            if i < len(variables)-1 and variables[i][0] == variables[i+1][0]:
+                exponent = 0
+                for j in range(2):
+                    if len(variables[i+j]) == 1:
+                        exponent += 1
+                    else:
+                        exponent += int(variables[i+j]
+                                        [variables[i+j].find('^')+1:])
+                simple_variable = '{}^{}'.format(variables[i][0], exponent)
+                variables.pop(i)
+            else:
+                simple_variable = variables[i]
+            simple_variables.append(simple_variable)
+            i += 1
+        return simple_variables
 
     def get_str(self):
         return self.string
--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -1,4 +1,6 @@
 from polynomial import Polynomial
+D = ['x', 'x', 's', 's', 'f']
+A = Polynomial('example')
 
 
 class TestPolynomial():
@@ -6,22 +8,42 @@ class TestPolynomial():
         a = ['y', 'x^3']
         b = Polynomial('example')
         c = ['x^23', '12', 'x^3y^3abc']
-        d = ['12', 'a^1212b', 'abcdg', '21']
+        d = ['12', '12A^1212b', 'abcdg', '21', 'bbcdg']
+        f = ['8k', '4h', '3k', '4']
         d.sort(key=b.sort_by_monomial)
         a.sort(key=b.sort_by_monomial)
         c.sort(key=b.sort_by_monomial)
+        f.sort(key=b.sort_by_monomial)
         assert a == ['y', 'x^3']
         assert c == ['12', 'x^3y^3abc', 'x^23']
-        assert d == ['12', '21', 'abcdg', 'a^1212b']
+        assert d == ['12', '21', 'bbcdg', 'abcdg', '12A^1212b']
+        assert f == ['4', '8k', '3k', '4h']
 
     def test_sort_by_variables(self):
-        a = Polynomial('example')
         b = ['12']
-        c = ['s', 'a^2', 'd']
-        d = ['x', 'y', 'x', 's', 's', 'f']
-        b.sort(key=a.sort_by_variables)
-        c.sort(key=a.sort_by_variables)
-        d.sort(key=a.sort_by_variables)
+        c = ['s', 'A^2', 'd']
+        b.sort(key=A.sort_by_variables)
+        c.sort(key=A.sort_by_variables)
+        D.sort(key=A.sort_by_variables)
         assert b == ['12']
-        assert c == ['a^2', 'd', 's']
-        print(d)
+        assert c == ['A^2', 'd', 's']
+        assert D == ['f', 's', 's', 'x', 'x']
+
+    def test_simplify_monomial(self):
+        c = A.simplify_monomial(D)
+        k = ['x^23', 'x^2', 'g', 's']
+        k = A.simplify_monomial(k)
+        assert c == ['f', 's^2', 'x^2']
+        assert k == ['x^25', 'g', 's']
+
+    def test_simplify(self):
+        pol1 = Polynomial('3k+4h+8k+4')
+        pol1.simplify()
+        assert pol1.get_str() == '4h+3k+8k+4'
+        pol2 = Polynomial('12+21+abcdg+13A^1212b')
+        assert pol2.get_str() == '13A^1212b+abcdg+33'
+
+    def test_get_monomial(self):
+        pol = Polynomial('3k+4h+8k+4')
+        monomials = pol.get_monomials(pol.string)
+        assert monomials == ['3k', '+4h', '+8k', '+4']
