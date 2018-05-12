f7f5fd7486578aa5e250b73d73ec9f28eb2fef20
Artem Gafetinov <artem.gafetinov@gmail.com>
2018-05-12 12:39:48 +0500

    some changing

--- polynomial.py
+++ polynomial.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Polynomial():
     def __init__(self, string):
         self.string = string
@@ -50,7 +53,6 @@ class Polynomial():
     def simplify(self):
         monomials = self.get_monomials(self.string)
         simple_monomials = []
-        monomial_index = 0
         for monomial in monomials:
             variables = []
             multipliers = [1]
@@ -62,29 +64,76 @@ class Polynomial():
                     while j < len(monomial) and monomial[j].isdigit():
                         multiplier += monomial[j]
                         j += 1
-                    multipliers.append(multiplier)
+                    multipliers.append(int(multiplier))
                     i = j - 1
                 elif monomial[i] == '-':
                     multipliers.append(-1)
                 elif monomial[i].isalpha():
                     variable = monomial[i]
                     if i < len(monomial)-1 and monomial[i+1] == '^':
-                        variable += monomial[i:i+2]
+                        variable += monomial[i+1:i+2]
                         j = i + 2
-                        while monomial[j].isdigit():
+                        while j < len(monomial) and monomial[j].isdigit():
                             variable += monomial[j]
                             j += 1
-                        i = j
+                        i = j-1
                     variables.append(variable)
                 i += 1
             variables.sort(key=self.sort_by_variables)
             variables = self.simplify_monomial(variables)
             variables.sort(key=self.sort_by_variables)
             common_multiplier = self.multiply(multipliers)
+            if common_multiplier == 1:
+                common_multiplier = ''
+            elif common_multiplier == -1:
+                common_multiplier = '-'
             simple_monomials.append(str(common_multiplier)+''.join(variables))
-            monomial_index += 1
+        simple_monomials.sort(key=self.sort_by_monomial, reverse=True)
+        simple_monomials = self.add_up_such_terms(simple_monomials)
+        simple_monomials = self.add_up_such_terms(simple_monomials)
         simple_monomials.sort(key=self.sort_by_monomial, reverse=True)
         self.string = '+'.join(simple_monomials)
+
+    def add_up_such_terms(self, pol):
+        i = 0
+        simple_pols = []
+        while i < len(pol):
+            if i < len(pol)-1 and \
+                    pol[i].lstrip('1234567890+-') ==\
+                    pol[i+1].lstrip('1234567890+-'):
+                multiplier = re.match(r'-?\d*', pol[i]).group(0)
+                if multiplier is None or multiplier == '':
+                    multiplier = 1
+                elif multiplier == '-':
+                    multiplier = -1
+                else:
+                    multiplier = int(multiplier)
+                while i < len(pol)-1 and \
+                        pol[i].lstrip('1234567890+-') ==\
+                        pol[i+1].lstrip('1234567890+-'):
+                    k2 = re.match(r'-?\d*', pol[i+1]).group(0)
+                    if k2 is None or k2 == '':
+                        k2 = 1
+                    elif k2 == '-':
+                        k2 = -1
+                    else:
+                        k2 = int(k2)
+                    multiplier += k2
+                    i += 1
+                if multiplier == 0:
+                    simple_pol = '0'
+                elif multiplier == 1:
+                    simple_pol = pol[i].lstrip('1234567890+-')
+                elif multiplier == -1:
+                    simple_pol = '-'+pol[i].lstrip('1234567890+-')
+                else:
+                    simple_pol = str(multiplier) + pol[i].lstrip(
+                        '1234567890+-')
+            else:
+                simple_pol = pol[i]
+            simple_pols.append(simple_pol)
+            i += 1
+        return simple_pols
 
     def simplify_monomial(self, variables):
         simple_variables = []
@@ -92,14 +141,20 @@ class Polynomial():
         while i < len(variables):
             if i < len(variables)-1 and variables[i][0] == variables[i+1][0]:
                 exponent = 0
-                for j in range(2):
-                    if len(variables[i+j]) == 1:
+                if len(variables[i]) == 1:
+                    exponent += 1
+                else:
+                    exponent += int(variables[i]
+                                    [variables[i].find('^') + 1:])
+                while i < len(variables)-1 and \
+                        variables[i][0] == variables[i+1][0]:
+                    if len(variables[i+1]) == 1:
                         exponent += 1
                     else:
-                        exponent += int(variables[i+j]
-                                        [variables[i+j].find('^')+1:])
+                        exponent += int(variables[i+1]
+                                        [variables[i+1].find('^')+1:])
+                    i += 1
                 simple_variable = '{}^{}'.format(variables[i][0], exponent)
-                variables.pop(i)
             else:
                 simple_variable = variables[i]
             simple_variables.append(simple_variable)
--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -1,4 +1,5 @@
 from polynomial import Polynomial
+import random
 D = ['x', 'x', 's', 's', 'f']
 A = Polynomial('example')
 
@@ -16,8 +17,8 @@ class TestPolynomial():
         f.sort(key=b.sort_by_monomial)
         assert a == ['y', 'x^3']
         assert c == ['12', 'x^3y^3abc', 'x^23']
-        assert d == ['12', '21', 'bbcdg', 'abcdg', '12A^1212b']
-        assert f == ['4', '8k', '3k', '4h']
+        assert d == ['12', '21', 'abcdg', 'bbcdg', '12A^1212b']
+        assert f == ['4', '4h', '8k', '3k']
 
     def test_sort_by_variables(self):
         b = ['12']
@@ -39,11 +40,50 @@ class TestPolynomial():
     def test_simplify(self):
         pol1 = Polynomial('3k+4h+8k+4')
         pol1.simplify()
-        assert pol1.get_str() == '4h+3k+8k+4'
+        assert pol1.get_str() == '11k+4h+4'
         pol2 = Polynomial('12+21+abcdg+13A^1212b')
+        pol2.simplify()
         assert pol2.get_str() == '13A^1212b+abcdg+33'
+        pol3 = Polynomial('a-a')
+        pol3.simplify()
+        assert pol3.string == '0'
+        pol4 = Polynomial('0')
+        pol4.simplify()
+        assert pol4.string == '0'
+        pol5 = Polynomial('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        pol5.simplify()
+        assert pol5.string == 'a^49'
+        pol5 = Polynomial('bbbbb'
+                          'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        pol5.simplify()
+        assert pol5.string == 'a^49b^5'
+        pol6 = Polynomial('2a-a+b-a-b')
+        pol6.simplify()
+        assert pol6.string == '0'
+        pol7 = Polynomial('2a-a+2-a')
+        pol7.simplify()
+        assert pol7.string == '2'
+        pol8 = Polynomial('3a-2a+2b-b-a')
+        pol8.simplify()
+        assert pol8.string == 'b'
+        pol9 = Polynomial('3a-2b-4a+2b')
+        pol9.simplify()
+        assert pol9.string == '2a'
 
     def test_get_monomial(self):
         pol = Polynomial('3k+4h+8k+4')
         monomials = pol.get_monomials(pol.string)
         assert monomials == ['3k', '+4h', '+8k', '+4']
+
+    def test_same_polynomial(self):
+        monomials = ['abcdg', '13A^1212b', '23', '4a',
+                     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                     'bbbbb'
+                     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                     'vnsdkvndskl']
+        for i in range(100):
+            random.shuffle(monomials)
+            string = '+'.join(monomials)
+            polinom = Polynomial(string)
+            polinom.simplify()
+            assert polinom.string == '13A^1212b+a^49b^5+a^49+d^2k^2ln^2s^2v^2+abcdg+4a+23'
